9425e3c9cdca1e3c27ee1197b972e704a8cde68c
Alin Chiosa <alin.c.wiredelta@gmail.com>
2023-03-05 14:35:52 +0200

    chore: created screen container component

---
+++ src/components/ScreenContainer.tsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import React, { HTMLAttributes } from "react";
+
+type ScreenContainerProps = HTMLAttributes<HTMLDivElement>;
+
+const ScreenContainer: React.FC<ScreenContainerProps> = (props) => {
+  const Container = styled.div`
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column; 
+    align-items: center:
+    box-sizing: border-box;
+
+    @media (max-width: 767px) {
+      padding: 0.5rem;
+    }
+
+    @media (min-width: 768px) and (max-width: 1023px) {
+      max-width: 600px;
+    }
+  `;
+
+  return <Container {...props} />;
+};
+
+export default ScreenContainer;

af93ea27df5db4091a3473e4428fc073b5c4f13c
Alin Chiosa <alin.c.wiredelta@gmail.com>
2023-03-06 17:10:51 +0200

    chore: updated colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,8 +1,10 @@
 const Colors = {
   DarkBlue: "#34536f",
   LightBlue: "#83a2c0",
-  White: "#FFFFFF",
+  White: "#ffffff",
   LowOpacityBlack: "rgba(0, 0, 0, 0.2)",
+  InactiveGrey: "#ced4da;",
+  Red: "#FF0000",
 };
 
 export default Colors;
